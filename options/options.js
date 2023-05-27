877205436783c7ca5ac215357a6cd0f9c04a0312
Crowdbotics <team@crowdbotics.com>
2023-05-27 07:50:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-limit-41034",
+  url: "https://restless-limit-41034.botics.co",
+  api: "https://restless-limit-41034.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
